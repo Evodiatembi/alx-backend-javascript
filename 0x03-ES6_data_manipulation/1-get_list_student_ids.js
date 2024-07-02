3a77464ab0e018f81495dd926961d4be89a39bcc
Evodiatembi <evodytembi@gmail.com>
2024-07-02 02:11:23 +0000

    1-get_list_student_ids.js

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -2,6 +2,6 @@ export default function getListStudentIds(array) {
   if (!Array.isArray(array)) {
 	  return [];
 	  }
-    return array.map((a) => a.id);
+    return array.map((array) => array.id);
   }
   
